5fd8e5aea38dbcc3d30d4b601573a4afa232523b
mehrnaz-ghaffar <mehrnaz.ghaffar@gmail.com>
2022-02-05 15:36:13 +0330

    chore : added tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
+    <nav>
+      <ul>
+        <li class="bg-pink-300">home</li>
+        <li>products</li>
+        <li>special offers</li>
+        <li>about</li>
 
-
+      </ul>
+    </nav>
     
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import './assets/tailwind.css'
 
 export default {
   name: 'App',
@@ -17,6 +25,5 @@ export default {
 }
 </script>
 
-<style>
+<style src="./assets/tailwind.css" />
 
-</style>
