c89e632002e64ca54e7ed0b55b70572c4fba5d22
mehhrnaz-ghaffar <mehrnaz.ghaffar@gmail.com>
2022-07-06 14:53:49 +0430

    worked on configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 module.exports = {
   purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  // darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12 , 88% , 59%)'
+      }
+    },
   },
-  variants: {
-    extend: {},
-  },
+  // variants: {
+  //   extend: {},
+  // },
   plugins: [],
 }
