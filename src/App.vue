1a55f9b1f0ab25cd616ab3256467d3f0c7084419
mehhrnaz-ghaffar <mehrnaz.ghaffar@gmail.com>
2022-07-09 22:21:31 +0430

    chore : finished navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
   <div id="app">
     <!-- Navbar -->
-    <nav class="relative container bg-pink-300">
-      <ul class="flex justify-around">
-        <li class="text-xl">home</li>
-        <li>products</li>
-        <li>special offers</li>
-        <li>about</li>
+    <nav class="relative container mx-auto p-6 bg-pink-300">
+      <!-- Flex container -->
+      <div class="flex items-center justify-between">
+        <!-- Logo -->
+        <div class="pt-2">
+          <img src="./img/logo.svg" alt="">
+        </div>
+        <!-- Menue items -->
+        <div class="hidden md:flex space-x-12">
+          <a href="#" class="hover:text-brightRed">home</a>
+          <a href="#" class="hover:text-brightRed">products</a>
+          <a href="#" class="hover:text-brightRed">special offers</a>
+          <a href="#" class="hover:text-brightRed">about</a>
+        </div>
+        
 
-      </ul>
+      </div>
+
+
+
+      
     </nav>
     
   </div>
